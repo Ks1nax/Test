ae5548cdf280e7fefde2d9bf6f180a174d882315
Ks1nax <scpfoudation.net@gmail.com>
2022-09-10 11:52:37 +0300

    Student simulator

---
+++ p2.py
@@ -0,0 +1,10 @@
+class Student:
+    print("hi!")
+    def __init__(self, height=160):
+        self.height = height
+        print("I am alive")
+
+first_student = Student()
+print(first_student.height)
+kate = Student(height=175)
+print(kate.height)
